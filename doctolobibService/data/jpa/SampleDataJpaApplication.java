b55435d3101e2af58fcef8defa6c5b84fa03b1a1
CecilebougmaIstic <cecile.bougma@etudiant.univ-rennes1.fr>
2021-09-21 15:25:30 +0200

    ajout de l'annotation Swagger

--- doctolobibService/data/jpa/SampleDataJpaApplication.java
+++ doctolobibService/data/jpa/SampleDataJpaApplication.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package sample.data.jpa;
+package doctolobibService.data.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
